453c6902d24a1313793d773caac7b30e84240268
vidush2004 <153823804+vidush2004@users.noreply.github.com>
2025-04-11 19:45:32 +0530

    cloudinary and middleware setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: process.env.COUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.COUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (filePath) => {
+    try {
+        if(!filePath){
+            console.log("FILE NOT FOUND at ", filePath);
+            return null;
+        }
+        const response = await cloudinary.uploader.upload(filePath, {
+            resource_type: "auto"
+        })
+        console.log("File has been uploaded successfully! \n URL: ", response.url);
+
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filePath) //remove the locally saved temp file on MY server
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
